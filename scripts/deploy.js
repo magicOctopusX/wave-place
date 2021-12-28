8d4bf081affb352c006a58b5d30d6574d4b3eed2
magicOctopusX <zhiyang@pundix.com>
2021-12-28 17:08:55 +0800

    deploy.js fund contract

---
+++ scripts/deploy.js
@@ -0,0 +1,22 @@
+const { ethers } = require("ethers")
+const { hexStripZeros } = require("ethers/lib/utils")
+
+async function main() {
+    const [deployer] = await ethers.getSigners()
+
+    console.log("Deploying contracts with the account:", deployer.address)
+    console.log("Account balance:", (await.deployer.getBalance()).toString())
+
+    const Token = await ethers.getContractFactory("WavePlace")
+    const token = await Token.deploy({value: hre.ethers.utils.parseEther("0.1")})
+    await token.deployed()
+
+    console.log("WavePlace address:", token.address)
+
+}
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
